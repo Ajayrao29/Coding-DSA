c081561be24dd69775ad767ada40830422b5ae2b
Ajayrao29 <ajayrao1294@gmail.com>
2024-10-18 23:47:56 +0530

    commit gcd

--- Mathematical_Based/01_Gcd.cpp.cpp
+++ Mathematical_Based/01_Gcd.cpp.cpp
@@ -1,11 +1,13 @@
-LEETCODE LINK :https://leetcode.com/problems/find-greatest-common-divisor-of-array
-GFG LINK      :https://www.geeksforgeeks.org/program-to-find-gcd-or-hcf-of-two-numbers
+// LEETCODE LINK : https://leetcode.com/problems/find-greatest-common-divisor-of-array
+// GFG LINK      : https://www.geeksforgeeks.org/program-to-find-gcd-or-hcf-of-two-numbers
 
-My Code :
+//My Code :
  
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
+        // Complexity O(n) 
+        // reason we are traversing the whole array to get the maximum and minimum value
         int n=nums.size();
         int maxi=nums[0];
         int mini=nums[0];
@@ -17,6 +19,8 @@ public:
                mini=nums[i];
            }
        }
+        // Complexity O(log(mini)) 
+        // reason the while loop is running till the mini is greater than 0 and in each iteration we are reducing the value of mini or maxi by a factor of 2
         while(maxi>0 && mini>0){
             if(maxi>mini){
                 maxi=maxi%mini;
@@ -30,3 +34,5 @@ public:
          return maxi;        
     }
 };
+
+
